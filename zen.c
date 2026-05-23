74a94476d445ec5e5b85326139a916bc180f6b75
Condition00 <anant.kavuru@gmail.com>
2026-05-23 20:50:54 +0530

    fallback

--- zen.c
+++ zen.c
@@ -1,6 +1,7 @@
 /*** includes ***/
 
 #include <asm-generic/errno-base.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -72,13 +73,37 @@ char editorReadKey() {
   return c;
 }
 
+// fallback method for getting the window size
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
+
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) !=
+        12) // fallback window size
       return -1;
-    editorReadKey();
-    return -1;
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -90,7 +115,11 @@ int getWindowSize(int *rows, int *cols) {
 void editorDrawRows() { // vim like ~'s
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDIN_FILENO, "~\r\n", 3);
+    write(STDIN_FILENO, "~", 1);
+
+    if (y < E.screenrows - 1) { // last line an exception to print a '~'
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
