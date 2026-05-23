7fa12091cd39a8264932eb44615e748f8b3838c8
Condition00 <anant.kavuru@gmail.com>
2026-05-23 15:48:19 +0530

    byte by byte input instead of line by line

--- zen.c
+++ zen.c
@@ -1,19 +1,29 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+
 void enableRawMode() {
-    struct termios raw;
 
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON); // icanon is a local flag and not a input
+                                   // flag (byte by byte input)
+                                   ////progaram will quit on 'q'
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-    enableRawMode();
-    
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    return 0;
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    ;
+  return 0;
 }
